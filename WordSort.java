181ac344f55cd4164f071b455fc5363b5d90eaff
lummish <lummish@gmail.com>
2014-10-23 23:05:20 -0700

    WordSort
    Used treemap as final output instead of a list

--- WordSort.java
+++ WordSort.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
+import java.util.TreeMap;
 
 public class WordSort {
 	public static void main( String[] args ) throws Exception {
@@ -12,7 +13,7 @@ public class WordSort {
 			new InputStreamReader(System.in));
 		Scanner src = new Scanner(stdIn);
 
-		CopyOnWriteArrayList<String> wordList = new CopyOnWriteArrayList<String>();
+		ArrayList<String> wordList = new ArrayList<String>();
 		int fromIndex = 0;
 		int toIndex = 0;
 		int count = 0;
@@ -20,12 +21,12 @@ public class WordSort {
 
 		src.useDelimiter("[.,;:\\n\\s-]+"); //sets delimeters
 
-		if ( args.length == 1 && "-sensitive".equals( args[0] ) ) { //case sensitive method
+		if ( args.length == 1 && "-sensitive".equals( args[0] ) ) { //case sensitive
 			while ( src.hasNext() ) { 
 				wordList.add( src.next() );
 			}
 		}
-		else if (args.length == 0) { //upper case method
+		else if (args.length == 0) { //upper case
 			while ( src.hasNext() ) { 
 				wordList.add( src.next().toUpperCase() );
 			}
@@ -37,19 +38,18 @@ public class WordSort {
 		Collections.sort( wordList );
 
 		Iterator<String> it = wordList.iterator();
+		TreeMap<String, Integer> tree = new TreeMap<String, Integer>();
+
 		while( it.hasNext() ) {
 			s = it.next();
 			fromIndex = wordList.indexOf( s );
 			toIndex = wordList.lastIndexOf( s );
 			count = toIndex - fromIndex + 1; 
 			if ( wordList.contains( s ) ) {
-				wordList.set( fromIndex, s + " " + count );
-			}
-			if ( it.hasNext() ) {
-				wordList.subList( fromIndex + 1, toIndex + 1 ).clear();
+				tree.put( s, count );
 			}
 		}
-		
-		System.out.println(wordList);
+
+		System.out.println( tree );
 	}
 }
